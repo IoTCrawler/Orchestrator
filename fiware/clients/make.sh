aa63d11a878452ec9e722f72dcb4d64ad65b1105
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 13:52:25 +0000

    Update make.sh

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -1,7 +1,7 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ "$1" = "prepare-djane" ]; then
 	rm -rf ~/djane
-	cd ~ && git clone https://github.com/sensinov/djane.git
+	git clone https://github.com/sensinov/djane.git ~/djane
 	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
 	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
 	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
@@ -23,7 +23,7 @@ fi
 if [ "$1" = "test-ngsi-ld-client" ]; then
     sh make.sh prepare-djane
     #docker network create djanenet &
-    docker rm $(docker ps | awk '{print $1}')
+    docker rm $(docker ps | awk '{print $1}') --force
     docker-compose -f ~/djane/docker-compose.yml up
     echo "Sleeping 15s before starting tests" && sleep 15
 	mvn -Dtest=NgsiLDClientTest test

b2a76e77f7ddf67b922d28c75f0a329d5189970c
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 13:09:59 +0100

    Changes in make.sh files

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -1,27 +1,29 @@
-if [ "$1" == "prepare-djane" ]; then
+if [ "$1" = "prepare-djane" ]; then
 	rm -rf ~/djane
 	cd ~ && git clone https://github.com/sensinov/djane.git
 	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
 	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
-	cd ~/djane && docker build . -t iotcrawler/djane:1.0.0
+	cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
 fi
 
-if [ "$1" == "install" ]; then
+if [ "$1" = "install" ]; then
 	mvn validate && mvn install -DskipTests=true
 fi
 
-if [ "$1" == "test-iot-broker-client" ]; then
+if [ "$1" = "test-iot-broker-client" ]; then
 	docker run -d -t -p 8065:8065 -p 8060:8060 fiware/iotbroker:standalone-dev -p iotbroker_historicalagent="enabled"
 	mvn test IoTBrokerClientTest#updateContextElement
 	mvn test IoTBrokerClientTest#queryContextElementTest
 fi
 
-if [ "$1" == "test-ngsi-ld-client" ]; then
-	docker run -d -p 3000:3000 iotcrawler/djane:1.0.0
+if [ "$1" = "test-ngsi-ld-client" ]; then
+    CID=$(docker run -d -p 3000:3000 gitlab.iotcrawler.net:4567/core/djane:1.0.0)
+	MID=$(docker run -d -p 27017:27017 -d mongo)
     echo "Sleeping 15s" && sleep 15
 	mvn -Dtest=NgsiLDClientTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#addEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#updateEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#getEntitiesTest surefire:test
-	#CID=$(docker ps | grep djane | awk '{print $1}') && echo "Stopping $CID" && docker stop $CID &
-fi
+    #echo "Stopping djane $CID" && docker stop $CID &
+    #echo "Stopping mongo $MID" && docker stop $MID &
+fi 
--- make.sh
+++ make.sh
@@ -1,16 +1,17 @@
-if [ "$1" == "install" ]; then
+#!/usr/bin/env bash
+if [ "$1" = "install" ]; then
 	cd fiware && sh install.sh && cd ../
 	cd core && mvn install -DskipTests=true	
 	#cd orchestrator && sh make.sh package
 fi
 
 
-if [ "$1" == "install-reqs" ]; then
+if [ "$1" = "install-reqs" ]; then
 	cd fiware/clients && sh make.sh prepare-djane
 	cd orchestrator && sh make.sh install-reqs
 fi
 
-if [ "$1" == "start" ]; then
+if [ "$1" = "start" ]; then
 	cd orchestrator && docker-compose up &
 	sleep 5
 	cd orchestrator && docker-compose up orchestrator
@@ -18,11 +19,11 @@ if [ "$1" == "start" ]; then
 	#cd orchestrator && make start &
 fi	
 
-if [ "$1" == "test-orchestrator" ]; then
+if [ "$1" = "test-orchestrator" ]; then
 	cd orchestrator && sh make.sh test
 fi
 
 
-if [ "$1" == "test-fiware-clients" ]; then
+if [ "$1" = "test-fiware-clients" ]; then
    	cd fiware/clients && sh make.sh test-ngsi-ld-client
-fi
+fi 

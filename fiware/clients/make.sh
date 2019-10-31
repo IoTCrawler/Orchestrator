481616a56f6216ac389a858df38549c2b11dc2b7
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 13:38:04 +0000

    Update make.sh

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -2,14 +2,12 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ "$1" = "prepare-djane" ]; then
 	rm -rf ~/djane
 	cd ~ && git clone https://github.com/sensinov/djane.git
-	cd ~/djane
-    ls
-    sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" models/entityModel.js
-	sed -i "s~let authentication=true;~let authentication=false;~g" config/config.js
-	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" config/config.js
-    #docker-compose build
-    cd ..
-	#cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
+	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
+	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
+	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
+	cat ~/djane/docker-compose.yml
+    docker-compose -f ~/djane/docker-compose.yml build
+    #cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
 fi
 
 if [ "$1" = "install" ]; then
@@ -27,11 +25,11 @@ if [ "$1" = "test-ngsi-ld-client" ]; then
     docker network create djanenet &
     #cd ~/djane && docker-compose up -d
     echo "Sleeping 15s before starting tests" && sleep 15
-	cd $__dir && mvn -Dtest=NgsiLDClientTest surefire:test
+	mvn -Dtest=NgsiLDClientTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#addEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#updateEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#getEntitiesTest surefire:test
-    cd ~/djane && docker-compose down
+    docker-compose -f ~/djane/docker-compose.yml down
 	#echo "Stopping djane $CID" && docker stop $CID &
     #echo "Stopping mongo $MID" && docker stop $MID &
 fi 

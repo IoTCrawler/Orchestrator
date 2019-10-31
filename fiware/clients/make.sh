2033e43a81ff552255e17b54e56cd718c9d8b49a
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 15:00:00 +0000

    Update make.sh

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -1,16 +1,11 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ "$1" = "prepare-djane" ]; then
-	#rm -rf ~/djane && sleep 2
-	#ls ~/djane
-	#git clone https://github.com/sensinov/djane.git ~/djane
-	mkdir -p ~/djane
-	wget https://raw.githubusercontent.com/sensinov/djane/master/docker-compose.yml -O ~/djane/docker-compose.yml
-	#sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
-	#sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
-	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
-	cat ~/djane/docker-compose.yml
-	#docker-compose -f ~/djane/docker-compose.yml build
-    
+	rm -rf ~/djane #not works on runner
+	git clone https://github.com/sensinov/djane.git ~/djane
+	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
+	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
+	#sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
+	docker-compose -f ~/djane/docker-compose.yml build
     #cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
 fi
 
@@ -25,17 +20,20 @@ if [ "$1" = "test-iot-broker-client" ]; then
 fi
 
 if [ "$1" = "test-ngsi-ld-client" ]; then
-    sh make.sh prepare-djane
+    #sh make.sh prepare-djane
     docker network create djanenet &
+    echo "Removing all running containers"
     docker rm $(docker ps | awk '{print $1}') --force
-    docker-compose -f ~/djane/docker-compose.yml up -d
+    echo "Pulling needed images"
+    docker-compose -f docker-compose.yml pull
+    docker-compose -f docker-compose.yml up -d
     echo "Sleeping 15s before starting tests" && sleep 15
 	mvn -Dtest=NgsiLDClientTest test
 	mvn -Dtest=IoTBrokerClientTest test
 	#mvn -Dtest=NgsiLDClientTest#addEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#updateEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#getEntitiesTest surefire:test
-    docker-compose -f ~/djane/docker-compose.yml down
+    docker-compose -f docker-compose.yml down
 	#echo "Stopping djane $CID" && docker stop $CID &
     #echo "Stopping mongo $MID" && docker stop $MID &
 fi 

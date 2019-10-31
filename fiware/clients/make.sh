e9bb573734e0ed46bec1ae9109e106c190f6eea3
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 14:28:59 +0100

    Testing djane with prebuilt image in docker-compose

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -1,11 +1,14 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 if [ "$1" = "prepare-djane" ]; then
 	rm -rf ~/djane
 	cd ~ && git clone https://github.com/sensinov/djane.git
-	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
-	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
-	cd ~/djane && docker-compose build
+	cd ~/djane
+    ls
+    sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" models/entityModel.js
+	sed -i "s~let authentication=true;~let authentication=false;~g" config/config.js
+	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" config/config.js
+    #docker-compose build
+    cd ..
 	#cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
 fi
 
@@ -31,3 +34,4 @@ if [ "$1" = "test-ngsi-ld-client" ]; then
 	#echo "Stopping djane $CID" && docker stop $CID &
     #echo "Stopping mongo $MID" && docker stop $MID &
 fi 
+

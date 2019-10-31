c32343dbea9098a7aadf2782acdc822dd672e920
Pavel Smirnov <psmirnov@agtinternational.com>
2019-10-31 13:55:18 +0000

    Update make.sh

--- fiware/clients/make.sh
+++ fiware/clients/make.sh
@@ -1,12 +1,13 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ "$1" = "prepare-djane" ]; then
-	rm -rf ~/djane
+	sudo rm -rf ~/djane
 	git clone https://github.com/sensinov/djane.git ~/djane
-	sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
-	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
-	sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
 	cat ~/djane/docker-compose.yml
-    docker-compose -f ~/djane/docker-compose.yml build
+	#sed -i "s~if (attributename == '@context')~if (attributename == '@context11')~g" ~/djane/models/entityModel.js
+	sed -i "s~let authentication=true;~let authentication=false;~g" ~/djane/config/config.js
+	#sed -i "s~build: .~image: gitlab.iotcrawler.net:4567/core/djane:1.0.0~g" ~/djane/docker-compose.yml
+	#docker-compose -f ~/djane/docker-compose.yml build
+    
     #cd ~/djane && docker build . -t gitlab.iotcrawler.net:4567/core/djane:1.0.0
 fi
 
@@ -24,13 +25,13 @@ if [ "$1" = "test-ngsi-ld-client" ]; then
     sh make.sh prepare-djane
     #docker network create djanenet &
     docker rm $(docker ps | awk '{print $1}') --force
-    docker-compose -f ~/djane/docker-compose.yml up
-    echo "Sleeping 15s before starting tests" && sleep 15
-	mvn -Dtest=NgsiLDClientTest test
+    #docker-compose -f ~/djane/docker-compose.yml up
+    #echo "Sleeping 15s before starting tests" && sleep 15
+	#mvn -Dtest=NgsiLDClientTest test
 	#mvn -Dtest=NgsiLDClientTest#addEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#updateEntityTest surefire:test
 	#mvn -Dtest=NgsiLDClientTest#getEntitiesTest surefire:test
-    docker-compose -f ~/djane/docker-compose.yml down
+    #docker-compose -f ~/djane/docker-compose.yml down
 	#echo "Stopping djane $CID" && docker stop $CID &
     #echo "Stopping mongo $MID" && docker stop $MID &
 fi 

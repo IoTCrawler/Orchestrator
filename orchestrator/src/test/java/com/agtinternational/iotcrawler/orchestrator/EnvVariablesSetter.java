52b4b4c58efe6378fcf2a21a1d007ca0cccf33db
psmirnov <psmirnov@agtinternational.com>
2019-11-08 14:57:27 +0100

    changes in env variables

--- orchestrator/src/test/java/com/agtinternational/iotcrawler/orchestrator/EnvVariablesSetter.java
+++ orchestrator/src/test/java/com/agtinternational/iotcrawler/orchestrator/EnvVariablesSetter.java
@@ -5,6 +5,7 @@ import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import static com.agtinternational.iotcrawler.core.Constants.IOTCRAWLER_RABBIT_HOST;
 import static com.agtinternational.iotcrawler.core.Constants.IOTCRAWLER_REDIS_HOST;
+import static com.agtinternational.iotcrawler.orchestrator.Constants.*;
 
 public class EnvVariablesSetter {
 
@@ -15,22 +16,29 @@ public class EnvVariablesSetter {
         //public static final String IoTCTripleStoreURI = "http://10.67.42.53:10035/repositories/IoTCrawler2/sparql";
         //public static final String defaultTripleStoreURI = "http://10.67.42.53:10035/repositories/KB/sparql";
 
-        environmentVariables.set(IOTCRAWLER_RABBIT_HOST, "rabbit");
+        if(!System.getenv().containsKey(IOTCRAWLER_RABBIT_HOST))
+            environmentVariables.set(IOTCRAWLER_RABBIT_HOST, "rabbit");
+
+            if(!System.getenv().containsKey(IOTCRAWLER_REDIS_HOST))
         environmentVariables.set(IOTCRAWLER_REDIS_HOST,  "redis");
 
 //        environmentVariables.set(Constants.TRIPLE_STORE_URI, "http://localhost:10035/repositories/MDR/sparql");
 //        environmentVariables.set(Constants.TRIPLE_STORE_USER, "test");
 //        environmentVariables.set(Constants.TRIPLE_STORE_PASS, "xyzzy");
 
-        //environmentVariables.set(Constants.NGSILD_BROKER_URI, "http://djane:3000/ngsi-ld/");
-        environmentVariables.set(Constants.NGSILD_BROKER_URI, "http://155.54.95.248:9090/ngsi-ld/");
-        //environmentVariables.set(Constants.IOT_BROKER_URI, "http://10.67.1.107:8060/ngsi10");
-        environmentVariables.set(Constants.IOT_BROKER_URI, "http://iotbroker:8060/ngsi10");
+        if(!System.getenv().containsKey(NGSILD_BROKER_URI))
+            //environmentVariables.set(Constants.NGSILD_BROKER_URI, "http://djane:3000/ngsi-ld/");
+            environmentVariables.set(NGSILD_BROKER_URI, "http://155.54.95.248:9090/ngsi-ld/");
 
-        environmentVariables.set(Constants.HTTP_SERVER_HOST, "10.67.1.107");
-        environmentVariables.set(Constants.HTTP_SERVER_PORT, "3001");
+        if(!System.getenv().containsKey(IOT_BROKER_URI))
+            //environmentVariables.set(Constants.IOT_BROKER_URI, "http://10.67.1.107:8060/ngsi10");
+            environmentVariables.set(IOT_BROKER_URI, "http://iotbroker:8060/ngsi10");
 
+        if(!System.getenv().containsKey(HTTP_SERVER_HOST))
+            environmentVariables.set(HTTP_SERVER_HOST, "10.67.1.107");
 
+        if(!System.getenv().containsKey(HTTP_SERVER_PORT))
+            environmentVariables.set(HTTP_SERVER_PORT, "3001");
 
         //public static final String ContextBrokerEndpoint = "http://localhost:1026/";
 

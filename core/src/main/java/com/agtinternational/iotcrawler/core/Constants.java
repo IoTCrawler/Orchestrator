063a4bfc7e1885206a1b2d649e4c56bfb6a49a99
psmirnov <psmirnov@agtinternational.com>
2019-11-08 15:43:06 +0100

    Env vars for gitlab

--- core/src/main/java/com/agtinternational/iotcrawler/core/Constants.java
+++ core/src/main/java/com/agtinternational/iotcrawler/core/Constants.java
@@ -5,15 +5,15 @@ public class Constants {
     public static final String IOTCRAWLER_RABBIT_HOST = "IOTCRAWLER_RABBIT_HOST";
     public static final String IOTCRAWLER_REDIS_HOST = "IOTCRAWLER_REDIS_HOST";
     public static final String IOTCRAWLER_COMMANDS_EXCHANGE = "IOTCRAWLER_COMMANDS_EXCHANGE";
-
-    public static final String TRIPLE_STORE_URI = "TRIPLE_STORE_URI";
-    public static final String TRIPLE_STORE_USER = "TRIPLE_STORE_USER";
-    public static final String TRIPLE_STORE_PASS = "TRIPLE_STORE_PASS";
-    public static final String IOT_BROKER_URI = "IOT_BROKER_URI";
-    public static final String NGSILD_BROKER_URI = "NGSILD_BROKER_URI";
-
-    public static final String HTTP_SERVER_HOST = "HTTP_SERVER_HOST";
-    public static final String HTTP_SERVER_PORT = "HTTP_SERVER_PORT";
+//
+//    public static final String TRIPLE_STORE_URI = "TRIPLE_STORE_URI";
+//    public static final String TRIPLE_STORE_USER = "TRIPLE_STORE_USER";
+//    public static final String TRIPLE_STORE_PASS = "TRIPLE_STORE_PASS";
+//    public static final String IOT_BROKER_URI = "IOT_BROKER_URI";
+//    public static final String NGSILD_BROKER_URI = "NGSILD_BROKER_URI";
+//
+//    public static final String HTTP_SERVER_HOST = "HTTP_SERVER_HOST";
+//    public static final String HTTP_SERVER_PORT = "HTTP_SERVER_PORT";
 
     public static final String sosaNS = "http://www.w3.org/ns/sosa/";
     public static final String sosaPrefix = "sosa";

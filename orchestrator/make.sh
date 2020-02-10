343341dfedb45345f8e97a6f8503856df8c2493d
psmirnov <psmirnov@agtinternational.com>
2020-02-10 17:36:26 +0100

    Minor changes for tests

---
+++ orchestrator/make.sh
@@ -0,0 +1,18 @@
+if [ "$1" = "rest-client-test" ]; then
+	export JAVA_TOOL_OPTIONS="-Xmx256m -Xms256m"
+	mvn package -DskipTests=true
+	export ORCHESTRATOR_ID=$(docker ps | grep orchestrator: | awk '{print $1}')
+	export ORCHESTRATOR_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.iotcrawler.IPAddress}}" ${ORCHESTRATOR_ID})
+	export IOTCRAWLER_ORCHESTRATOR_IP=http://${ORCHESTRATOR_IP}:3001/ngsi-ld/
+	mvn -Dtest=OrchestratorTestsREST surefire:test
+fi
+
+#if [ "$1" = "rpc-client-test" ]; then
+#rpc-client-test:
+#	export JAVA_TOOL_OPTIONS="-Xmx256m -Xms256m"
+#	export IOTCRAWLER_RABBIT_HOST=localhost
+#	export IOTCRAWLER_REDIS_HOST=localhost
+#	export NGSILD_BROKER_URL=http://localhost:3000/ngsi-ld/
+#	mvn package -DskipTests=true
+#	mvn -Dtest=OrchestratorTestsRPC surefire:test
+#fi

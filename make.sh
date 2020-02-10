9afb50736eabf76ea5b24e9fe109e3ad061f46b7
psmirnov <psmirnov@agtinternational.com>
2020-02-10 13:17:03 +0100

    changes in makefiles

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ echo ${CI_PROJECT_DIR}
 
 if [ "$1" = "install" ]; then
 	export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
-	cd ${CI_PROJECT_DIR}/fiware && make install
+	cd ${CI_PROJECT_DIR}/fiware && sh make.sh install
 	cd ${CI_PROJECT_DIR}/core && mvn install -DskipTests=true
 	cd ${CI_PROJECT_DIR}/orchestrator && make package
 fi

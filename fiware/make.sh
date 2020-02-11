f2094c07c0368496ab763156737fa1e73603dbbf
psmirnov <psmirnov@agtinternational.com>
2020-02-11 11:40:30 +0100

    Minor changes for CI/CD

--- fiware/make.sh
+++ fiware/make.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
 echo ${CI_PROJECT_DIR}
+set -e
 
 if [ "$1" = "install" ]; then
 	cd $CI_PROJECT_DIR/fiware/models && make install

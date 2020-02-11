80bd2948aa48ab7154be4bbda743f26a45f65d31
psmirnov <psmirnov@agtinternational.com>
2020-02-11 15:25:13 +0100

    Fix in fiware-clients

--- make.sh
+++ make.sh
@@ -5,10 +5,10 @@ set -e
 
 if [ "$1" = "install" ]; then
 	export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
-  # Main: Checking IoTCrawler fiware dependencies
+  echo "Main: Checking IoTCrawler fiware dependencies"
 	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware ]; then cd ${CI_PROJECT_DIR}/fiware && sh make.sh install; fi);
-	# Main: Checking IoTCrawler core dependency
+	echo "Main: Checking IoTCrawler core dependency"
 	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ]; then cd ${CI_PROJECT_DIR}/core && mvn install -DskipTests=true; fi);
-	# Main: Packaging orchestrator
+	echo "Main: Packaging orchestrator"
 	cd ${CI_PROJECT_DIR}/orchestrator && make package
 fi

ffabf9d0db9b9948f026a13039e4eabd45db7b33
psmirnov <psmirnov@agtinternational.com>
2020-02-21 10:58:26 +0100

    CommitID

--- make.sh
+++ make.sh
@@ -16,6 +16,7 @@ if [ "$1" = "install" ]; then
 fi
 
 if [ "$1" = "push-image" ]; then
+  echo "# Setting env vars for pushing"
   if [[ -z "$CI_COMMIT_TAG" ]]; then
         export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG}
         export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_SHA}
@@ -23,6 +24,7 @@ if [ "$1" = "push-image" ]; then
         export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE}
         export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_TAG}
   fi
+  echo "# Pushing image to registry"
   docker push "$CI_APPLICATION_REPOSITORY:$CI_APPLICATION_TAG"
 	#docker push "$CI_APPLICATION_REPOSITORY:latest"
 fi

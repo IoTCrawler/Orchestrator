50d1b18ced8ea928329323cc5b830ed57d27208f
psmirnov <psmirnov@agtinternational.com>
2020-03-02 15:53:55 +0100

    Deployment changes

--- orchestrator/make.sh
+++ orchestrator/make.sh
@@ -24,13 +24,13 @@ fi
 
 if [ "$1" = "push-image" ]; then
 #  echo "# Setting env vars for pushing"
-#  if [ -z "$CI_COMMIT_TAG" ]; then
-#        export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG}
-#        export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_SHA}
-#      else
-#        export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE}
-#        export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_TAG}
-#  fi
+  if [ -z "$CI_COMMIT_TAG" ]; then
+        export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE/$CI_COMMIT_REF_SLUG}
+        export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_SHA}
+      else
+        export CI_APPLICATION_REPOSITORY=${CI_APPLICATION_REPOSITORY:-$CI_REGISTRY_IMAGE}
+        export CI_APPLICATION_TAG=${CI_APPLICATION_TAG:-$CI_COMMIT_TAG}
+  fi
 #  if [[ -n "$CI_REGISTRY" && -n "$CI_REGISTRY_USER" ]]; then
 #    echo "Logging to GitLab Container Registry with CI credentials..."
 #    docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"

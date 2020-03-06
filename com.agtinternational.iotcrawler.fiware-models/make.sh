18a1429c2c79e65a48298baf3e4e815299c834fb
psmirnov <psmirnov@agtinternational.com>
2020-03-06 15:38:41 +0100

    Changes in fiware-models

---
+++ com.agtinternational.iotcrawler.fiware-models/make.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "prepare-ngsi2-api" ]; then
+	(if [ ! -d /tmp/fiware-ngsi2-api ]; then git clone https://gitlab+deploy-token-8:GcnYwtYSFLcAVsyKoUEZ@gitlab.iotcrawler.net/orchestrator/fiware-ngsi2-api.git /tmp/fiware-ngsi2-api; fi);
+	cd /tmp/fiware-ngsi2-api && mvn install -DskipTests=true
+	cd /tmp/fiware-ngsi2-api/ngsi2-client && mvn install -DskipTests=true
+fi
+
+if [ "$1" = "install" ]; then
+    #Fiware/models: checking ngsi2 dependency
+	(if [ ! -d ~/.m2/repository/com/orange/fiware/ngsi2-client ]; then sh make.sh prepare-ngsi2-api; fi);
+
+  # Fiware-models: checking com/agtinternational/iotcrawler
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ]; then cd "${CI_PROJECT_DIR}/IoTCrawler" && mvn install; fi);
+	#Fiware/models: installing
+	mvn validate && mvn install -DskipTests=true
+fi

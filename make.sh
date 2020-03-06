bef48a6782ae210bd75b8b567aabfbd9f8aa4e1d
psmirnov <psmirnov@agtinternational.com>
2020-03-06 15:45:10 +0100

    Changes in general

--- make.sh
+++ make.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
-echo ${CI_PROJECT_DIR}
+echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 set -e
+
 
 if [ "$1" = "install" ]; then
 	export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
-  echo "# Main: Installing IoTCrawler fiware dependencies"
-	#(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware ]; then cd ${CI_PROJECT_DIR}/fiware && sh make.sh install; fi);
-	cd ${CI_PROJECT_DIR}/fiware && sh make.sh install
-	echo "# Main: Installing IoTCrawler core dependency"
-	#(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ]; then cd ${CI_PROJECT_DIR}/core && mvn install -DskipTests=true; fi);
-	cd ${CI_PROJECT_DIR}/core && mvn install -DskipTests=true
-	echo "# Main: Packaging orchestrator"
-	cd ${CI_PROJECT_DIR}/orchestrator && make package
+  echo "# Main: checking com/agtinternational/iotcrawler"
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ]; then cd "${CI_PROJECT_DIR}/IoTCrawler" && mvn install; fi);
+	echo "# Main: checking com/agtinternational/iotcrawler/fiware-models"
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models" && sh make.sh install; fi);
+	echo "# Main: checking com/agtinternational/iotcrawler/fiware-clients"
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-clients" && sh make.sh install; fi);
+  echo "# Main: checking com/agtinternational/iotcrawler/core"
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.core" && mvn install -DskipTests=true; fi);
 fi

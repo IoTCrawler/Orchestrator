8eaee76b8c4474d3e9a1b2314aa2c55a8b62b154
psmirnov <psmirnov@agtinternational.com>
2020-03-10 15:55:53 +0100

    Rebuild_ALL added

--- make.sh
+++ make.sh
@@ -7,12 +7,12 @@ set -e
 if [ "$1" = "install" ]; then
 	export CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(pwd)}
   echo "# Main: checking com/agtinternational/iotcrawler"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ]; then cd "${CI_PROJECT_DIR}/IoTCrawler" && mvn install; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ] || [ -n "$REBUILD_ALL" ]; then cd "${CI_PROJECT_DIR}/IoTCrawler" && mvn install; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-models"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models" && sh make.sh install; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models" && sh make.sh install; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-clients"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-clients" && sh make.sh install; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ] || [ -n "$REBUILD_ALL" ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-clients" && sh make.sh install; fi);
   echo "# Main: checking com/agtinternational/iotcrawler/core"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.core" && mvn install -DskipTests=true; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ] || [ -n "$REBUILD_ALL" ]; then cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.core" && mvn install -DskipTests=true; fi);
 	sh ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator/make.sh install
 fi

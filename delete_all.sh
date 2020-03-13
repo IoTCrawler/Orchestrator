eab1b7d0dda40cf0edcc688409cbc36a4c85737b
psmirnov <psmirnov@agtinternational.com>
2020-03-13 13:13:39 +0100

    Moving to nginx-ingress

---
+++ delete_all.sh
@@ -0,0 +1,5 @@
+kubectl delete --all configmaps --namespace orchestrator-3-staging
+kubectl delete --all deployments --namespace orchestrator-3-staging
+kubectl delete --all services --namespace orchestrator-3-staging
+kubectl delete --all ingresses --namespace orchestrator-3-staging
+

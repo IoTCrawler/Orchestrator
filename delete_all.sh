0495ef001397fd0fe3637c044fc4c7ff5c9485e1
psmirnov <psmirnov@agtinternational.com>
2020-03-13 18:35:27 +0100

    Deployment changes

--- delete_all.sh
+++ delete_all.sh
@@ -1,5 +1,6 @@
-kubectl delete --all configmaps --namespace orchestrator-3-staging
-kubectl delete --all deployments --namespace orchestrator-3-staging
-kubectl delete --all services --namespace orchestrator-3-staging
-kubectl delete --all ingresses --namespace orchestrator-3-staging
+kubectl delete --all configmaps --namespace orchestrator-3-production
+kubectl delete --all deployments --namespace orchestrator-3-production
+kubectl delete --all services --namespace orchestrator-3-production
+kubectl delete service ingress-nginx --namespace ingress-nginx
+kubectl delete configmap tcp-services --namespace ingress-nginx
 

527ed78156b756cb7d9dad5a461255354c58d54d
Pavel Smirnov <psmirnov@agtinternational.com>
2020-03-16 16:55:08 +0100

    Changes in chart

--- delete_all.sh
+++ delete_all.sh
@@ -1,6 +1,9 @@
-kubectl delete --all configmaps --namespace orchestrator-3-production
-kubectl delete --all deployments --namespace orchestrator-3-production
-kubectl delete --all services --namespace orchestrator-3-production
-kubectl delete service ingress-nginx --namespace ingress-nginx
-kubectl delete configmap tcp-services --namespace ingress-nginx
-
+#!/usr/bin/env bash
+NS=$1
+kubectl delete --all services --namespace $NS ||
+kubectl delete --all deployments --namespace $NS ||
+kubectl delete --all pods --namespace $NS ||
+kubectl delete --all replicaset --namespace $NS ||
+kubectl delete service ingress-nginx --namespace ingress-nginx ||
+kubectl delete configmap tcp-services --namespace ingress-nginx ||
+kubectl get all --namespace ingress-nginx

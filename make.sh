896c93afc385dad144cb30cecce36327bdf5f4c0
Pavel Smirnov <psmirnov@agtinternational.com>
2020-03-16 17:36:25 +0100

    Changes in build scripts

--- make.sh
+++ make.sh
@@ -9,18 +9,18 @@ if [ "$1" = "install" ]; then
   echo "# Main: checking com/agtinternational/iotcrawler"
 	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ] || [ -n "$REBUILD_ALL" ]; then cd "${CI_PROJECT_DIR}/IoTCrawler" && mvn install; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-models"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models && sh make.sh install; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models && sh make.sh install && cd ..; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-clients"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-clients && sh make.sh install; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-clients && sh make.sh install && cd ..; fi);
   echo "# Main: checking com/agtinternational/iotcrawler/core"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.core && mvn install -DskipTests=true; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ] || [ -n "$REBUILD_ALL" ]; then cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.core && mvn install -DskipTests=true && cd ..; fi);
 	sh ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator/make.sh install
 fi
 
 if [ "$1" = "build-image" ]; then
-  cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator && sh make.sh build-image
+  cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator && sh make.sh build-image && cd ..
 fi
 
 if [ "$1" = "push-image" ]; then
-  cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator && sh make.sh push-image
+  cd ${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.orchestrator && sh make.sh push-image && cd ..
 fi

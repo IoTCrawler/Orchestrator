6d98a5503920bece0f44ef7d90ef8275975ebd4f
Pavel Smirnov <psmirnov@agtinternational.com>
2020-03-16 18:03:52 +0100

    Changes in build scripts

--- com.agtinternational.iotcrawler.fiware-clients/make.sh
+++ com.agtinternational.iotcrawler.fiware-clients/make.sh
@@ -26,7 +26,7 @@ if [ "$1" = "install" ]; then
 	echo "#Fiware/clients: Checking iot-broker dependency"
 	(if [ ! -d ~/.m2/repository/eu/neclab/iotplatform/iotbroker.client ] || [ -n "$REBUILD_ALL" ]; then sh make.sh prepare-iotbroker; fi);
 	echo "#Fiware/clients: Checking fiware-models dependency"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then CURR=$(pwd) cd ../com.agtinternational.iotcrawler.fiware-models && sh make.sh install && cd com.agtinternational.iotcrawler.fiware-clients; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then CURR=$(pwd) && cd "${CI_PROJECT_DIR}/com.agtinternational.iotcrawler.fiware-models" && sh make.sh install && cd $CURR; fi);
 	echo "#Fiware/clients: installing"
 	mvn install -DskipTests=true
 fi

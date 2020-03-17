a4932059b790ef47ce542c44346c9c9707bb8ab8
Pavel Smirnov <psmirnov@agtinternational.com>
2020-03-17 17:36:57 +0100

    Ingress-nginx moved to main chart

--- chart-ingress-nginx/install.sh
+++ chart-ingress-nginx/install.sh
@@ -1,2 +1,2 @@
-helm install ./chart-ingress-nginx --name ingress-nginx --namespace production-orchestrator-3-production
+helm install ./chart-ingress-nginx --name ingress-nginx --namespace orchestrator-3-production
 kubectl get service ingress-nginx --namespace production-orchestrator-3-production

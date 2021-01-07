017f04b8fa26c7d9fa48d6318852d0779567299d
Pavel Smirnov <psmirnov@agtinternational.com>
2021-01-07 14:42:59 +0100

    Improve make.sh

--- make.sh
+++ make.sh
@@ -6,16 +6,16 @@ set -e
 
 if [ "$1" = "install" ]; then
 
-  (if [ -n "$REBUILD_ALL" ]; then echo "rm -rf ~/.m2/repository/com/agtinternational/iotcrawler" && rm -rf ~/.m2/repository/com/agtinternational/iotcrawler; fi);
+  #(if [ -n "$REBUILD_ALL" ]; then echo "rm -rf ~/.m2/repository/com/agtinternational/iotcrawler" && rm -rf ~/.m2/repository/com/agtinternational/iotcrawler; fi);
 
   echo "# Main: checking com/agtinternational/iotcrawler"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ]; then cd "IoTCrawler" && mvn install && cd ..; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler ] || [ -n "$REBUILD_ALL" ]; then cd "IoTCrawler" && mvn install && cd ..; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-models"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ]; then cd "com.agtinternational.iotcrawler.fiware-models" && sh make.sh install && cd ..; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-models ] || [ -n "$REBUILD_ALL" ]; then cd "com.agtinternational.iotcrawler.fiware-models" && sh make.sh install && cd ..; fi);
 	echo "# Main: checking com/agtinternational/iotcrawler/fiware-clients"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ]; then cd "com.agtinternational.iotcrawler.fiware-clients" && sh make.sh install && cd ..; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/fiware-clients ] || [ -n "$REBUILD_ALL" ]; then cd "com.agtinternational.iotcrawler.fiware-clients" && sh make.sh install && cd ..; fi);
   echo "# Main: checking com/agtinternational/iotcrawler/core"
-	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ]; then cd "com.agtinternational.iotcrawler.core" && mvn install -DskipTests=true && cd ..; fi);
+	(if [ ! -d ~/.m2/repository/com/agtinternational/iotcrawler/core ] || [ -n "$REBUILD_ALL" ]; then cd "com.agtinternational.iotcrawler.core" && mvn install -DskipTests=true && cd ..; fi);
 	cd "com.agtinternational.iotcrawler.orchestrator" && sh make.sh install && cd ..
 fi
 
